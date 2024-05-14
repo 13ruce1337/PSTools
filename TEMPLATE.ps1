edbaf58e3f1aba220ce38aedaa73c810a1579f83
13ruce1337 <tagcoop@gmail.com>
2024-05-14 14:42:33 -0400

    updated readme to have copy button

---
+++ TEMPLATE.ps1
@@ -0,0 +1,57 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER <Parameter_Name>
+
+.INPUTS
+
+.OUTPUTS
+
+.NOTES
+  Version:        1.0
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  
+.LINK
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#$ErrorActionPreference = "SilentlyContinue"
+$ProgressPreference = $false
+
+#Dot Source required Function Libraries
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+[float]$script:script_version = 1.0
+
+#Log File Info
+[string]$script:log_path = "C:\Windows\Temp"
+[string]$script:log_name = "<script_name>.log"
+[string]$script:log_file = Join-Path -Path $script:log_path -ChildPath $script:log_name
+
+#Create Log File
+[void]$(New-Item -Path $log_file -ItemType "file" -Force -Value "[$(Get-Date -Format "HH:mm:ss")]: Log file created.")
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+function Log {
+  param(
+    [Parameter(Mandatory=$true)][String]$private:msg
+  )
+
+  [string]$private:time = Get-Date -Format "HH:mm:ss"
+  [string]$private:text = "[$private:time]:$private:msg"
+  
+  Write-Host $private:text
+  Add-Content -Path $script:log_file -Value "$private:text"
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
